1a0158d94e9d2d45be20e41bd1f6846aa8c5a3f0
irvan4iot <52144823+irvan4iot@users.noreply.github.com>
2023-08-27 15:53:18 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let img = document.getElementById("mingkem");
+const audio = new Audio("./src/pop.mp3")
+let score = document.getElementById("score");
+let count = 0;
+
+function increaseScore(){
+    count++;
+    score.innerHTML = count;
+}
+
+addEventListener("mousedown", function(){
+    increaseScore()
+    img.src = "./src/mangap.png";
+    audio.play();
+});
+
+addEventListener("mouseup", function(){
+    img.src = "./src/mingkem.png";
+    audio.play();
+});
+
+addEventListener("touchstart", function(){
+    img.src = "./src/mangap.png"
+    audio.play();
+});
+
+addEventListener("touchend", function(){
+    img.src = "./src/mingkem.png"
+    audio.play();
+});
